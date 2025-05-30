37e9890c7e3e803dcd910ae45d95477af38933b8
JaimeCAST69 <20223tn088@utez.edu.mx>
2025-05-29 20:51:23 -0600

    [FIRST COMMIT] subida inicial del proyecto

---
+++ src/main/java/utez/edu/mx/cv_jcr_9a2/CvJcr9a2Application.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_jcr_9a2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvJcr9a2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvJcr9a2Application.class, args);
+    }
+
+}
---
+++ src/test/java/utez/edu/mx/cv_jcr_9a2/CvJcr9a2ApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_jcr_9a2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvJcr9a2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
